d8ade18f6f313f2d0d9bb4b22fa44b111796024e
James Aimonetti <james@2600hz.com>
2021-09-01 11:02:34 -0700

    ignore functions not supported anymore

--- src/couchbeam_util.erl
+++ src/couchbeam_util.erl
@@ -282,9 +282,9 @@ proxy_token(Secret,UserName) ->
 hmac(Alg, Key, Data) ->
     case {Alg, erlang:function_exported(crypto, hmac, 3)} of
         {_, true} ->
-            crypto:hmac(Alg, Key, Data);
+            crypto:mac(hmac, Alg, Key, Data);
         {sha, false} ->
-            crypto:sha_mac(Key, Data);
+            crypto:mac(hmac, sha, Key, Data);
         {Alg, false} ->
             throw({unsupported, Alg})
     end.
